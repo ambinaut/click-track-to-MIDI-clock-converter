268945ecb4faf60ea83fbc58c287386ce10ec60f
ambinaut <ambinaut@users.noreply.github.com>
2017-04-20 00:24:06 +0300

    Update Pulse to MIDI - A.Comp (Up_01).cpp

--- Pulse to MIDI - A.Comp (Up_01).cpp
+++ Pulse to MIDI - A.Comp (Up_01).cpp
@@ -1,11 +1,12 @@
 /*
-Click track to MIDI clock converter.
+Click track to MIDI clock converter. Version 1.0
 For atmega328/168/88/48 with 16MHz clock.
 Tempo change needs a reset.
 Tempo range 40 - ca. 240 BPM.
 Input click quarter note.
 More info at: 
 https://ambinaut.wordpress.com/2017/04/19/click-track-to-midi-clock-converter/
+https://github.com/ambinaut/click-track-to-MIDI-clock-converter
 */
 
 #define F_CPU 16000000UL
@@ -36,7 +37,7 @@ beginning:
 	counter0_OCF0A_flags = 0;
 	TCNT0 = 0; // clear Timer0 Register
 	TIFR0 = (1 << OCF0A);//Clear Timer0 Output Compare A Match Flag (logical 1)
-	ACSR = (1 << ACI); //Clear • Bit 4 – ACI: Analog Comparator Interrupt Flag
+	ACSR = (1 << ACI); //Clear â€¢ Bit 4 â€“ ACI: Analog Comparator Interrupt Flag
 
 	//wait for first audio/tap tempo click/pulse
 	while (!readbit(ACSR, ACI)){} //Comparator Flag
@@ -71,7 +72,7 @@ beginning:
 								TCNT1 = 0;
 								F8_msgs_sent = 1;
 								TIFR1 |= (1 << OCF1A); //clear OCF1A compare flag (16bit)
-								//set value in the OCR1AH and OCR1AL– Output Compare Register 1 A
+								//set value in the OCR1AH and OCR1ALâ€“ Output Compare Register 1 A
 								OCR1AH = (byte)(counter0_OCF0A_flags >> 8);
 								OCR1AL = (byte)(counter0_OCF0A_flags);					
 								goto pulsed;
@@ -123,7 +124,7 @@ pulsed:
 					
 				} //END if (readbit(TIFR1, OCF1A)) - Compare Flag (16bit)
 
-			//• Bit 4 – ACI: Analog Comparator Interrupt Flag
+			//â€¢ Bit 4 â€“ ACI: Analog Comparator Interrupt Flag
 			//i.e. Tap Pulse
 		if (readbit(ACSR, ACI)) //next click detected
 			{
@@ -134,7 +135,7 @@ pulsed:
 						F8_msgs_sent = 1;		
 						blink_Led();
 						//ACSR = (1 << ACI); //Clear ACI Flag , (tap pulse); WRONG! 200ms Limit!
-						//clear Bit 4 – OCF1A: Timer/Counter1, Output Compare A - Match Flag
+						//clear Bit 4 â€“ OCF1A: Timer/Counter1, Output Compare A - Match Flag
 						setbit(TIFR1, OCF1A); //Clear the compare flag [16 bit timer]
 					}	
 			} //end if (readbit(ACSR, ACI))
@@ -180,42 +181,42 @@ void init_io(){
 	setbit(PORT_MIDIstart, sw_MIDIstart_number);	
 	
 	//Disable digital buffers on AIN0/AIN1
-	//DIDR1 – Digital Input Disable Register 1
+	//DIDR1 â€“ Digital Input Disable Register 1
 	//Optional, reduces power consumption.
 	setbit(DIDR1, AIN0D);
 	setbit(DIDR1, AIN1D);
 	/*
-	• Bit 1, 0 – AIN1D, AIN0D: AIN1, AIN0 Digital Input Disable
+	â€¢ Bit 1, 0 â€“ AIN1D, AIN0D: AIN1, AIN0 Digital Input Disable
 	When this bit is written logic one, the digital input buffer on the AIN1/0 pin is disabled. The corresponding
 	PIN Register bit will always read as zero when this bit is set. When an analog signal is
 	applied to the AIN1/0 pin and the digital input from this pin is not needed, this bit should be written
 	logic one to reduce power consumption in the digital input buffer.
 	*/
 	
-	//ACSR – Analog Comparator Control and Status Register
+	//ACSR â€“ Analog Comparator Control and Status Register
 	setbit(ACSR, ACIS1); //Comparator Interrupt on Falling Output Edge.
 	/*
-	• Bits 1, 0 – ACIS1, ACIS0: Analog Comparator Interrupt Mode Select
+	â€¢ Bits 1, 0 â€“ ACIS1, ACIS0: Analog Comparator Interrupt Mode Select
 	These bits determine which comparator events that trigger the Analog Comparator 
 	interrupt. The different settings are shown in Table 22-2.
 	*/	
 	
 	//Timer/Counter 0 - (8 Bit)
-	//TCCR0A – Timer/Counter Control Register A
+	//TCCR0A â€“ Timer/Counter Control Register A
 	setbit(TCCR0A, WGM01); //CTC OCRA Immediate MAX
-	//TCCR0B – Timer/Counter Control Register B
+	//TCCR0B â€“ Timer/Counter Control Register B
 	TCCR0B |= (1<<CS01) | (1<<CS00); // clk/64 - one tick = 4uS @16MHz system clock
-	//OCR0A – Output Compare Register A
+	//OCR0A â€“ Output Compare Register A
 	OCR0A = 23;  //Counting from 0 to 23, i.e. 24 * 4uS @ F_osc(16MHz)/64	
-	//TIFR0 – Timer/Counter 0 Interrupt Flag Register
-	//• Bit 1 – OCF0A: Timer/Counter 0 Output Compare A Match Flag
+	//TIFR0 â€“ Timer/Counter 0 Interrupt Flag Register
+	//â€¢ Bit 1 â€“ OCF0A: Timer/Counter 0 Output Compare A Match Flag
 	TIFR0 = (1 << OCF0A);//Clear the compare flag (inverted latch)
 	//One MIDI Clock interval = OCF0A matches * 4uS
 	
 	//Timer/Counter 1 - 16bit 
 	TCCR1A = 0;     // clear Timer1 TCCR1A register
 	TCCR1B = 0;     // clear Timer1 TCCR1B register
-	//TCCR1B – Timer/Counter 1 Control Register B
+	//TCCR1B â€“ Timer/Counter 1 Control Register B
 	//setbit(TCCR1B, ICES1); //Input Capture Pin (ICP1/PB0) trigger capture event on rising edge
 	setbit(TCCR1B, WGM12); //Clear Timer on Compare Match (CTC) mode - CTC OCR1A Immediate MAX
 	setbit(TCCR1B, CS11); // must set to pre-scaler 64 (4uS/tick) otherwise overflow on output compare register
